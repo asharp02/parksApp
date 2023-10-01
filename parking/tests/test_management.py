7bbde48a1958766a6fa88632da15b2dbbfb2ae06
Adrian Sharpe <adriansharpe148@gmail.com>
2023-10-01 15:00:10 -0400

    Add two unit tests for the get_parking_dump mgmt command

---
+++ parking/tests/test_management.py
@@ -0,0 +1,28 @@
+import os
+
+from django.test import TestCase, SimpleTestCase
+from whereToPark.management.commands.get_parking_dump import Command
+
+# Create your tests here.
+
+
+def file_exists(directory, filename):
+    root_path = os.getcwd()
+    file_path = os.path.join(root_path, directory, filename)
+    return os.path.exists(file_path)
+
+
+class GetParkingDumpTests(SimpleTestCase):
+    def test_fetch_data_folder_outputs_zipped_folder(self):
+        Command().fetch_data_folder()
+        directory = ""
+        filename = "parking_schedules.zip"
+        self.assertTrue(file_exists(directory, filename))
+
+    def test_unzip_files_in_right_folder(self):
+        Command().unzip_files()
+        directory = "whereToPark/fixtures"
+        no_parking_filename = "no_parking.xml"
+        restricted_parking_filename = "restricted_parking.xml"
+        self.assertTrue(file_exists(directory, no_parking_filename))
+        self.assertTrue(file_exists(directory, restricted_parking_filename))
