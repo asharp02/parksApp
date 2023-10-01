b8b36740c72acb83884b7add52e15c947360f23a
Adrian Sharpe <adriansharpe148@gmail.com>
2023-10-01 19:03:51 -0400

    Create Parking model and add tests to verify field labels

---
+++ parking/tests/test_models.py
@@ -0,0 +1,65 @@
+from django.test import TestCase
+
+from whereToPark.models import ParkingByLaw
+
+
+class ParkingByLawModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # setup objects used by all test methods
+        ParkingByLaw.objects.create(
+            bylaw_no="[Repealed 2016-04-05 by By-law No. 365-2016]",
+            source_id="1",
+            schedule="15",
+            schedule_name="Parking for Restricted Periods",
+            highway="Ashbury Avenue",
+            side="North",
+            between="Glenholme Avenue and Oakwood Avenue",
+            times_and_or_days="10:00 a.m. to 6:00 p.m., Mon. to Fri.",
+            max_period_permitted="12 hours",
+        )
+
+    def test_bylaw_no_label(self):
+        law = ParkingByLaw.objects.get(id=1)
+        field_label = law._meta.get_field("bylaw_no").verbose_name
+        self.assertEqual(field_label, "bylaw no")
+
+    def test_source_id(self):
+        law = ParkingByLaw.objects.get(id=1)
+        field_label = law._meta.get_field("source_id").verbose_name
+        self.assertEqual(field_label, "source id")
+
+    def test_schedule_label(self):
+        law = ParkingByLaw.objects.get(id=1)
+        field_label = law._meta.get_field("schedule").verbose_name
+        self.assertEqual(field_label, "schedule")
+
+    def test_schedule_name_label(self):
+        law = ParkingByLaw.objects.get(id=1)
+        field_label = law._meta.get_field("schedule_name").verbose_name
+        self.assertEqual(field_label, "schedule name")
+
+    def test_highway_label(self):
+        law = ParkingByLaw.objects.get(id=1)
+        field_label = law._meta.get_field("highway").verbose_name
+        self.assertEqual(field_label, "highway")
+
+    def test_side_label(self):
+        law = ParkingByLaw.objects.get(id=1)
+        field_label = law._meta.get_field("side").verbose_name
+        self.assertEqual(field_label, "side")
+
+    def test_between_label(self):
+        law = ParkingByLaw.objects.get(id=1)
+        field_label = law._meta.get_field("between").verbose_name
+        self.assertEqual(field_label, "between")
+
+    def test_times_and_or_day_label(self):
+        law = ParkingByLaw.objects.get(id=1)
+        field_label = law._meta.get_field("times_and_or_days").verbose_name
+        self.assertEqual(field_label, "times and or days")
+
+    def test_max_period_permitted_label(self):
+        law = ParkingByLaw.objects.get(id=1)
+        field_label = law._meta.get_field("max_period_permitted").verbose_name
+        self.assertEqual(field_label, "max period permitted")
--- parking/whereToPark/models.py
+++ parking/whereToPark/models.py
@@ -5,19 +5,19 @@ STREET_SIDES = (("W", "West"), ("E", "East"), ("N", "North"), ("S", "South"))
 
 
 # Create your models here.
-# class ParkingByLaws(models.Model):
-#     """
-#     This model represents two datasets provided by City of Toronto: "No Parking"
-#     and "Parking for Restricted Periods". Each record will represent a bylaw
-#     that specifies either the time range/location of where someone can or cant park.
-#     """
+class ParkingByLaw(models.Model):
+    """
+    This model represents two datasets provided by City of Toronto: "No Parking"
+    and "Parking for Restricted Periods". Each record will represent a bylaw
+    that specifies either the time range/location of where someone can or cant park.
+    """
 
-#     id = models.IntegerField()
-#     side_of_road = models.CharField(max_length=10, choices=STREET_SIDES)
-#     road_name = models.CharField(max_length=100)
-#     boundaries = models.CharField(max_length=100)  # Consider using long/lat values
-#     times_specified = models.CharField(
-#         max_length=100
-#     )  # range representing parking period
-#     can_park = models.BooleanField()
-#     max_period_permitted = models.PositiveSmallIntegerField()  # in hours
+    bylaw_no = models.CharField(max_length=100)
+    source_id = models.CharField(max_length=10)
+    schedule = models.CharField(max_length=10)
+    schedule_name = models.CharField(max_length=100)
+    highway = models.CharField(max_length=100)
+    side = models.CharField(max_length=10)
+    between = models.CharField(max_length=200)
+    times_and_or_days = models.CharField(max_length=200)
+    max_period_permitted = models.CharField(max_length=100)
