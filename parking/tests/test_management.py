401f21d1e8da14955d7d26ae40d1a7fd7fc1bb3e
Adrian Sharpe <adriansharpe148@gmail.com>
2023-10-01 19:38:25 -0400

    Add additional model to represent both sets of parking by law data

--- parking/tests/test_management.py
+++ parking/tests/test_management.py
@@ -1,7 +1,9 @@
 import os
+import xml.etree.ElementTree as ET
 
 from django.test import TestCase, SimpleTestCase
 from whereToPark.management.commands.get_parking_dump import Command
+from whereToPark.models import NoParkingByLaw, RestrictedParkingByLaw
 
 # Create your tests here.
 
@@ -26,3 +28,17 @@ class GetParkingDumpTests(SimpleTestCase):
         restricted_parking_filename = "restricted_parking.xml"
         self.assertTrue(file_exists(directory, no_parking_filename))
         self.assertTrue(file_exists(directory, restricted_parking_filename))
+
+
+class ImportParkingDataTests(TestCase):
+    def test_model_count_matches_xml_files(self):
+        pass
+        # no_parking_tree = ET.parse("fixtures/no_parking.xml")
+        # no_parking_root = no_parking_tree.getroot()
+
+        # restricted_parking_tree = ET.parse("fixtures/restricted_parking.xml")
+        # print(len(no_parking_root.getchildren()))
+        # ParkingByLaw.objects.count()
+        # self.assertEqual(ParkingByLaw.objects.count())
+
+        # restricted_tree = ET.parse("fixtures/restricted_parking.xml")
--- parking/tests/test_models.py
+++ parking/tests/test_models.py
@@ -1,13 +1,75 @@
 from django.test import TestCase
 
-from whereToPark.models import ParkingByLaw
+from whereToPark.models import NoParkingByLaw, RestrictedParkingByLaw
 
 
-class ParkingByLawModelTest(TestCase):
+class NoParkingByLawModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         # setup objects used by all test methods
-        ParkingByLaw.objects.create(
+        NoParkingByLaw.objects.create(
+            bylaw_no="[Repealed 2016-04-05 by By-law No. 365-2016]",
+            source_id="1",
+            schedule="15",
+            schedule_name="Parking for Restricted Periods",
+            highway="Ashbury Avenue",
+            side="North",
+            between="Glenholme Avenue and Oakwood Avenue",
+            times_and_or_days="10:00 a.m. to 6:00 p.m., Mon. to Fri.",
+            prohibited_times_and_or_days="12 hours",
+        )
+
+    def test_bylaw_no_label(self):
+        law = NoParkingByLaw.objects.get(id=1)
+        field_label = law._meta.get_field("bylaw_no").verbose_name
+        self.assertEqual(field_label, "bylaw no")
+
+    def test_source_id(self):
+        law = NoParkingByLaw.objects.get(id=1)
+        field_label = law._meta.get_field("source_id").verbose_name
+        self.assertEqual(field_label, "source id")
+
+    def test_schedule_label(self):
+        law = NoParkingByLaw.objects.get(id=1)
+        field_label = law._meta.get_field("schedule").verbose_name
+        self.assertEqual(field_label, "schedule")
+
+    def test_schedule_name_label(self):
+        law = NoParkingByLaw.objects.get(id=1)
+        field_label = law._meta.get_field("schedule_name").verbose_name
+        self.assertEqual(field_label, "schedule name")
+
+    def test_highway_label(self):
+        law = NoParkingByLaw.objects.get(id=1)
+        field_label = law._meta.get_field("highway").verbose_name
+        self.assertEqual(field_label, "highway")
+
+    def test_side_label(self):
+        law = NoParkingByLaw.objects.get(id=1)
+        field_label = law._meta.get_field("side").verbose_name
+        self.assertEqual(field_label, "side")
+
+    def test_between_label(self):
+        law = NoParkingByLaw.objects.get(id=1)
+        field_label = law._meta.get_field("between").verbose_name
+        self.assertEqual(field_label, "between")
+
+    def test_times_and_or_day_label(self):
+        law = NoParkingByLaw.objects.get(id=1)
+        field_label = law._meta.get_field("times_and_or_days").verbose_name
+        self.assertEqual(field_label, "times and or days")
+
+    def test_max_period_permitted_label(self):
+        law = NoParkingByLaw.objects.get(id=1)
+        field_label = law._meta.get_field("prohibited_times_and_or_days").verbose_name
+        self.assertEqual(field_label, "prohibited times and or days")
+
+
+class RestrictedParkingByLawModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # setup objects used by all test methods
+        RestrictedParkingByLaw.objects.create(
             bylaw_no="[Repealed 2016-04-05 by By-law No. 365-2016]",
             source_id="1",
             schedule="15",
@@ -20,46 +82,46 @@ class ParkingByLawModelTest(TestCase):
         )
 
     def test_bylaw_no_label(self):
-        law = ParkingByLaw.objects.get(id=1)
+        law = RestrictedParkingByLaw.objects.get(id=1)
         field_label = law._meta.get_field("bylaw_no").verbose_name
         self.assertEqual(field_label, "bylaw no")
 
     def test_source_id(self):
-        law = ParkingByLaw.objects.get(id=1)
+        law = RestrictedParkingByLaw.objects.get(id=1)
         field_label = law._meta.get_field("source_id").verbose_name
         self.assertEqual(field_label, "source id")
 
     def test_schedule_label(self):
-        law = ParkingByLaw.objects.get(id=1)
+        law = RestrictedParkingByLaw.objects.get(id=1)
         field_label = law._meta.get_field("schedule").verbose_name
         self.assertEqual(field_label, "schedule")
 
     def test_schedule_name_label(self):
-        law = ParkingByLaw.objects.get(id=1)
+        law = RestrictedParkingByLaw.objects.get(id=1)
         field_label = law._meta.get_field("schedule_name").verbose_name
         self.assertEqual(field_label, "schedule name")
 
     def test_highway_label(self):
-        law = ParkingByLaw.objects.get(id=1)
+        law = RestrictedParkingByLaw.objects.get(id=1)
         field_label = law._meta.get_field("highway").verbose_name
         self.assertEqual(field_label, "highway")
 
     def test_side_label(self):
-        law = ParkingByLaw.objects.get(id=1)
+        law = RestrictedParkingByLaw.objects.get(id=1)
         field_label = law._meta.get_field("side").verbose_name
         self.assertEqual(field_label, "side")
 
     def test_between_label(self):
-        law = ParkingByLaw.objects.get(id=1)
+        law = RestrictedParkingByLaw.objects.get(id=1)
         field_label = law._meta.get_field("between").verbose_name
         self.assertEqual(field_label, "between")
 
     def test_times_and_or_day_label(self):
-        law = ParkingByLaw.objects.get(id=1)
+        law = RestrictedParkingByLaw.objects.get(id=1)
         field_label = law._meta.get_field("times_and_or_days").verbose_name
         self.assertEqual(field_label, "times and or days")
 
     def test_max_period_permitted_label(self):
-        law = ParkingByLaw.objects.get(id=1)
+        law = RestrictedParkingByLaw.objects.get(id=1)
         field_label = law._meta.get_field("max_period_permitted").verbose_name
         self.assertEqual(field_label, "max period permitted")
