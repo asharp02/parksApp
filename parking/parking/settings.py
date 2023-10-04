39327876ff663cdc9be574648a69d7b45c349edf
Adrian Sharpe <adriansharpe148@gmail.com>
2023-10-04 16:21:44 -0400

    Setup templates for initial prototype of map

--- parking/parking/settings.py
+++ parking/parking/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "parking.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["./whereToPark/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
