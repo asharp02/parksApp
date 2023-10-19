d89ea675ccfa14e3423ce9856cbaa7520440c08b
Adrian Sharpe <adriansharpe148@gmail.com>
2023-10-19 12:20:50 -0400

    Add drf serializers and viewsets for bylaws

---
+++ parking/api/serializers.py
@@ -0,0 +1,39 @@
+from whereToPark.models import ByLaw, Intersection, Highway
+from rest_framework import serializers
+
+
+class HighwaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Highway
+        fields = ["name"]
+
+
+class IntersectionSerializer(serializers.ModelSerializer):
+    main_street = HighwaySerializer(read_only=True)
+    cross_street = HighwaySerializer(read_only=True)
+
+    class Meta:
+        model = Intersection
+        fields = ["main_street", "cross_street", "lat", "lng"]
+
+
+class ByLawSerializer(serializers.ModelSerializer):
+    boundary_start = IntersectionSerializer(read_only=True)
+    boundary_end = IntersectionSerializer(read_only=True)
+    midpoint = serializers.ReadOnlyField()
+
+    class Meta:
+        model = ByLaw
+        fields = [
+            "source_id",
+            "schedule",
+            "schedule_name",
+            "highway",
+            "side",
+            "boundary_start",
+            "boundary_end",
+            "between",
+            "times_and_or_days",
+            "max_period_permitted",
+            "midpoint",
+        ]
---
+++ parking/api/views.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from api.serializers import (
+    ByLawSerializer,
+    HighwaySerializer,
+    IntersectionSerializer,
+)
+from whereToPark.models import ByLaw
+
+
+class NPByLawViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint that No Parking Bylaws to be viewed.
+    """
+
+    queryset = ByLaw.objects.get_np_bylaws_to_display().order_by("source_id")
+    serializer_class = ByLawSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class RPByLawViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint that Restricted Parking Bylaws to be viewed.
+    """
+
+    queryset = ByLaw.objects.get_rp_bylaws_to_display().order_by("source_id")
+    serializer_class = ByLawSerializer
+    permission_classes = [permissions.IsAuthenticated]
