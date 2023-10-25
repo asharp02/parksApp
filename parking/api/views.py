7815d7fe6ef003de185b3864649e3dcd2c4615e9
Adrian Sharpe <adriansharpe148@gmail.com>
2023-10-25 15:48:35 -0400

    Make api view enpoint read only for unauthenticated users

--- parking/api/views.py
+++ parking/api/views.py
@@ -16,7 +16,7 @@ class NPByLawViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = ByLaw.objects.get_np_bylaws_to_display().order_by("source_id")
     serializer_class = ByLawSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class RPByLawViewSet(viewsets.ReadOnlyModelViewSet):
@@ -26,4 +26,4 @@ class RPByLawViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = ByLaw.objects.get_rp_bylaws_to_display().order_by("source_id")
     serializer_class = ByLawSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
