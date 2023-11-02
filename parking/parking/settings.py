d8d87624e27ae342f8d0ef76254bcc3c8e60c18c
Adrian Sharpe <adriansharpe148@gmail.com>
2023-11-02 17:14:13 -0400

    more cors updates

--- parking/parking/settings.py
+++ parking/parking/settings.py
@@ -131,4 +131,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 1000,
 }
-CORS_ORIGIN_WHITELIST = ["http://localhost:5173", "https://parking.adriansh.com"]
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:5173",
+    "https://street-parking-toronto.vercel.app",
+]
